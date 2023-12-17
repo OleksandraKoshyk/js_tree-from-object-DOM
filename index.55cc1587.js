b0d9488c08c43028e641f6f7f61d531aaf094e60
Oleksandra Koshyk <sakoshik1984@gmail.com>
2023-12-17 18:05:57 +0100

    make build

---
+++ index.55cc1587.js
@@ -0,0 +1,2 @@
+!function e(n,t){let r=document.createElement("ul");for(let l in n.append(r),t){let n=document.createElement("li");n.innerText=l,r.append(n),Object.keys(t[l]).length&&e(n,t[l])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.55cc1587.js.map
